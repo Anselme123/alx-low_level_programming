c9ad344b721818955ee670d1e1e152ff32f8c08d
Anselme123 <nanselme35@gmail.com>
2023-08-24 04:15:21 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,14 @@
 
 void print_most_numbers(void)
 {
-
 char c;
-for (c = '0'; c = '9'; c++)
 
+for (c = '0'; c <= '9'; c++)
 {
 if (!(c == '2' || c == '4'))
-_putchar (n);
-_putchar ('\n');
+_putchar(c);
 }
-_putchar ('\n');
+_putchar('\n');
 }
+
+
